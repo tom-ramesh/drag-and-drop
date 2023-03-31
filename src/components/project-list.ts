d7d101a897cd3788c7a2aa4dc4c562d59577a4bb
tom-ramesh <tomramesh92@gmail.com>
2023-03-31 09:11:16 +0530

    log added in project-list

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -30,6 +30,7 @@ export class ProjectList
 
   @autobind
   dropHandler(event: DragEvent): void {
+    console.log("test");
     const prjId = event.dataTransfer!.getData("text/plain");
     console.log(prjId, this.type);
     projectState.moveProject(
